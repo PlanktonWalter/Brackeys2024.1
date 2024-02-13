8f885f1fe998cbd715e896edd94ee0fa1d694d9e
Malik <malik.muhametchin@gmail.com>
2024-02-13 17:48:54 +0100

    add and setup safe model

--- Assets/Scripts/Player/LockButton.cs
+++ Assets/Scripts/Player/LockButton.cs
@@ -4,28 +4,41 @@ using DG.Tweening;
 public sealed class LockButton : MonoBehaviour
 {
 
-    [SerializeField] private Transform _child;
-
     private Tween _currentTween;
+    private float _originalPositionZ;
+    private float _selectedPositionZ;
+    private float _pressedPositionZ;
+
+    private void Start()
+    {
+        _originalPositionZ = transform.localPosition.z - 0.01f;
+        _selectedPositionZ = _originalPositionZ + 0.01f;
+        _pressedPositionZ = _originalPositionZ - 0.005f;
+
+        transform.localPosition = new Vector3()
+        {
+            x = transform.localPosition.x,
+            y = transform.localPosition.y,
+            z = _originalPositionZ,
+        };
+    }
 
     public void OnSelected()
     {
-        //_child.localPosition = Vector3.forward * -0.4f;
         _currentTween?.Kill();
-        _currentTween = _child.DOLocalMoveZ(-0.2f, 0.05f);
+        _currentTween = transform.DOLocalMoveZ(_selectedPositionZ, 0.05f);
     }
 
     public void OnDeselected()
     {
-        //_child.localPosition = Vector3.zero;
         _currentTween?.Kill();
-        _currentTween = _child.DOLocalMoveZ(0f, 0.05f);
+        _currentTween = transform.DOLocalMoveZ(_originalPositionZ, 0.05f);
     }
 
     public void OnPressed()
     {
         _currentTween?.Kill();
-        _currentTween = _child.DOLocalMoveZ(0.1f, 0.2f);
+        _currentTween = transform.DOLocalMoveZ(_pressedPositionZ, 0.2f);
     }
 
 }
